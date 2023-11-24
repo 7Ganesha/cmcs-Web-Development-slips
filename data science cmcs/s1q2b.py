97abe9460cedc27c0041ff06568af9d33b36bd92
7Ganesha <122626239+7Ganesha@users.noreply.github.com>
2023-11-24 16:58:34 +0530

    Delete data science cmcs directory

--- data science cmcs/s1q2b.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load winequality-red data
-wine_data = pd.read_csv('winequality-red.csv')
-
-# View basic statistical details
-basic_stats = wine_data.describe()
-print(basic_stats)
